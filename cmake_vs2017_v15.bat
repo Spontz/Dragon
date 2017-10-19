0395056fa88e0c2a881fd10102fed5b89473b221
Iván Fernández <ifernandez@redsauce.net>
2017-10-19 23:02:31 +0200

    Project renamed to D R A G O N

---
+++ cmake_vs2017_v15.bat
@@ -0,0 +1,16 @@
+REM VS15 X86_32
+
+rmdir dragon_vs15_x86_32 /s /q
+mkdir dragon_vs15_x86_32
+cd dragon_vs15_x86_32
+start cmake.exe -G "Visual Studio 15" -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_UNICODE:BOOL=TRUE ..\
+cd..
+
+
+REM VS15 X86_64
+
+rmdir dragon_vs15_x86_64 /s /q
+mkdir dragon_vs15_x86_64
+cd dragon_vs15_x86_64
+start cmake.exe -G "Visual Studio 15 Win64" -DBUILD_SHARED_LIBS:BOOL=FALSE -DBUILD_UNICODE:BOOL=TRUE ..\
+cd..
