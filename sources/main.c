f4c21a4a675797f2b897da719837eac88e72c1f7
xphere <xpherezn@gmail.com>
2018-07-01 00:33:03 +0200

    + small change on the traces messages

--- sources/main.c
+++ sources/main.c
@@ -7,33 +7,32 @@
 // ******************************************************************
 
 int main(int argc, char *argv[]) {
-	dkernel_trace("Dragon: Starting up...\n");
+	dkernel_trace("Dragon Engine: Starting up...");
 
 	dkernel_getArguments(argc, argv);
-	dkernel_trace("Dragon: Got arguments\n");
 	
 	// Remove a previous camera.cam file in order to start capturing the camera from zero
 	// In the future, the captured camera values will be sent to the demo editor instead of being written to a file
 	remove("camera.cam");
-	dkernel_trace("Dragon: Cleaning old camera files");
+	dkernel_trace("Cleaning old camera files");
 	
 	dkernel_loadScripts();
-	dkernel_trace("Dragon: Scripts Loaded\n");
+	dkernel_trace("Scripts Loaded");
 	
 	if (demoSystem.slaveMode) {
-		dkernel_trace("Dragon: Running in network slave mode\n");
+		dkernel_trace("Running in network slave mode");
 	} else {
-		dkernel_trace("Dragon: Running in standalone mode\n");
+		dkernel_trace("Running in standalone mode");
 	}
 		
 	dkernel_initDemo();
-	dkernel_trace("Dragon: Engine inited successfully\n");
+	dkernel_trace("Engine inited successfully\n");
 
 	dkernel_mainLoop();
-	dkernel_trace("Dragon: Main loop ended successfully\n");
+	dkernel_trace("Main loop ended successfully\n");
 
 	dkernel_closeDemo();
-	dkernel_trace("Dragon: Engine closed\n");
+	dkernel_trace("Engine closed\n");
 
 	return EXIT_SUCCESS;
 }
