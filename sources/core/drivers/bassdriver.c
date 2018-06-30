15651062f555a1bcc9bc270c6383e2ddb47e7846
xphere <xpherezn@gmail.com>
2018-06-30 12:06:48 +0200

    Added BASS library support

---
+++ sources/core/drivers/bassdriver.c
@@ -0,0 +1,63 @@
+/*
+	bassdriver.c: bass interface
+*/
+
+#include "../../main.h"
+#include <bass.h>
+#include "bassdriver.h"
+
+
+// ******************************************************************
+
+void sound_init() {
+	int driver;
+	driver = BASS_Init(-1, 44100, 0, 0, NULL);
+	if (!driver) {
+		dkernel_error("bassdriver: Sound cannot be initialized, error in sound_init(), line BASS_Init()\n");
+		return;
+	}
+}
+
+// ******************************************************************
+
+void sound_play() {
+	BASS_Start();
+}
+
+// ******************************************************************
+
+void sound_update() {
+	BASS_Update(200);
+}
+
+// ******************************************************************
+
+void sound_pause() {
+	BASS_Pause();
+}
+
+// ******************************************************************
+
+void sound_stop() {
+	BASS_Stop();
+}
+
+// ******************************************************************
+
+void sound_end() {
+	BASS_Free();
+}
+
+// ******************************************************************
+
+float sound_cpu() {
+	return BASS_GetCPU();
+}
+
+// ******************************************************************
+
+int sound_channels() {
+	// TODO: To be implemented in BASS, as it was in FMOD
+	//return FSOUND_GetChannelsPlaying();
+	return 0;	
+}
